a35fe936fd7a4b55ccdc7a5b9895eb71ef230202
Milena Macedo <milena.macedo@mv.com.br>
2023-12-13 09:53:02 -0300

    first commit

---
+++ src/main/java/com/macedo/CrudSpringApplication.java
@@ -0,0 +1,14 @@
+package com.macedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+		System.out.println("Milena  Santos Macedo");
+	}
+
+}
