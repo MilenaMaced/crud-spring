e25733a08be6685225641d483b9e8b1af9cc298d
Milena Macedo <milena.macedo@mv.com.br>
2023-12-14 10:07:30 -0300

    Aula 21 - Função de cadastro de curso

--- src/main/java/com/macedo/controller/CourseController.java
+++ src/main/java/com/macedo/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.macedo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.macedo.model.Course;
@@ -8,19 +10,20 @@ import com.macedo.repository.CourseRepository;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/courses")
 public class CourseController {
 
-    
     private final CourseRepository courseRepository;
 
-    public CourseController(CourseRepository courseRepository){
+    public CourseController(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
 
@@ -29,4 +32,9 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
 }
