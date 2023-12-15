be10b2a3aa8d77cba2c8fd72b810e2de96dac515
Milena Macedo <milena.macedo@mv.com.br>
2023-12-15 17:21:43 -0300

    Aula 40 - Tratando Exceções com Controller Advice

--- src/main/java/com/macedo/controller/CourseController.java
+++ src/main/java/com/macedo/controller/CourseController.java
@@ -14,7 +14,6 @@ import jakarta.validation.constraints.Positive;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,9 +41,8 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseService.findById(id).map(course -> ResponseEntity.ok().body(course))
-                .orElse(ResponseEntity.notFound().build());
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseService.findById(id);
     }
 
     @PostMapping
@@ -54,18 +52,14 @@ public class CourseController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable @Valid @NotNull @Positive Long id, @RequestBody Course course) {
-        return courseService.update(id, course)
-                .map(recordFound -> {
-                    return ResponseEntity.ok().body(recordFound);
-                }).orElse(ResponseEntity.notFound().build());
+    public Course update(@PathVariable @Valid @NotNull @Positive Long id, @RequestBody Course course) {
+        return courseService.update(id, course);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        if (courseService.delete(id))
-            return ResponseEntity.noContent().<Void>build();
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        courseService.delete(id);
     }
 
 }
--- src/main/java/com/macedo/service/CourseService.java
+++ src/main/java/com/macedo/service/CourseService.java
@@ -1,10 +1,9 @@
 package com.macedo.service;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
+import com.macedo.exception.RecordNotFoundException;
 import com.macedo.model.Course;
 import com.macedo.repository.CourseRepository;
 
@@ -21,30 +20,27 @@ public class CourseService {
         return courseRepository.findAll();
     }
 
-    public Optional<Course> findById(Long id) {
-        return courseRepository.findById(id);
+    public Course findById(Long id) {
+        return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     public Course create(Course course) {
         return courseRepository.save(course);
     }
 
-    public Optional<Course> update(Long id, Course course) {
+    public Course update(Long id, Course course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     recordFound.setName(course.getName());
                     recordFound.setCategory(course.getCategory());
                     return courseRepository.save(recordFound);
 
-                });
+                }).orElseThrow(() -> new RecordNotFoundException(id));
+
     }
 
-    public boolean delete(Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return true;
-                }).orElse(false);
+    public void delete(Long id) {
+        courseRepository.delete(courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id)));
     }
-
 }
