e25da9e43a937f214f4904c3453c58b7dbc25c62
Milena Macedo <milena.macedo@mv.com.br>
2023-12-18 10:52:28 -0300

    Aula 41 - Criação de um DTO de curso

---
+++ src/main/java/com/macedo/dto/CourseDTO.java
@@ -0,0 +1,13 @@
+package com.macedo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CourseDTO(
+        Long id,
+        @NotBlank @NotNull @Size(min = 5, max = 100) String name,
+        @NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
+
+}
