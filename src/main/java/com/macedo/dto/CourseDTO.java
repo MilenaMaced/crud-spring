72c2a720c7a21145277211a5b246ab039171d749
Milena Macedo <milena.macedo@mv.com.br>
2023-12-20 14:58:08 -0300

    Aula 57 - Validando Enums com java

--- src/main/java/com/macedo/dto/CourseDTO.java
+++ src/main/java/com/macedo/dto/CourseDTO.java
@@ -4,16 +4,17 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.util.*;
 
+import com.macedo.enums.Category;
+import com.macedo.enums.validation.ValueOfEnum;
 
 public record CourseDTO(
-                Long id,
-                @NotBlank @NotNull @Size(min = 5, max = 100) String name,
-                @NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
-                @NotEmpty @NotNull @Valid List<LessonDTO> lessons) {
+        Long id,
+        @NotBlank @NotNull @Size(min = 5, max = 100) String name,
+        @NotNull @Size(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
+        @NotEmpty @NotNull @Valid List<LessonDTO> lessons) {
 
 }
